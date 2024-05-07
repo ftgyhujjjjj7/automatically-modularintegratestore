7c583751769537672d87fc02d651bb88cc7e088d
ftgyhujjjjj7 <js1aidtrain@hotmail.com>
2024-05-07 21:52:21 +0700

    start

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
-  }
-  return nums[left];
-}
+const dropRightWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
+  return arr;
+};
